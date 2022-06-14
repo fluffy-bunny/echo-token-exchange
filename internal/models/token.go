22f7dd1b92dccb2c849add49434455874627e8ee
Herb Stahl <ghstahl@gmail.com>
2022-06-14 16:20:47 -0700

    Herb refresh grace (#7)
    * added grace settings
    
    * working refresh grace
    
    * Apply genny and mockgen changes
    
    Co-authored-by: ghstahl <ghstahl@users.noreply.github.com>

--- internal/models/token.go
+++ internal/models/token.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type (
 	TokenMetadata struct {
@@ -16,13 +19,17 @@ type (
 		Data     map[string]interface{} `json:"data" mapstructure:"data"`
 	}
 	RefreshTokenInfo struct {
-		ClientID           string            `json:"client_id" mapstructure:"client_id"`
-		Subject            string            `json:"subject" mapstructure:"subject"`
-		Scope              string            `json:"scope" mapstructure:"scope"`
-		GrantType          string            `json:"grant_type" mapstructure:"grant_type"`
-		Params             map[string]string `json:"params" mapstructure:"params"`
-		Expiration         time.Time         `json:"expiration" mapstructure:"expiration"`
-		AbsoluteExpiration time.Time         `json:"absolute_expiration" mapstructure:"absolute_expiration"`
+		ClientID                     string            `json:"client_id" mapstructure:"client_id"`
+		Subject                      string            `json:"subject" mapstructure:"subject"`
+		Scope                        string            `json:"scope" mapstructure:"scope"`
+		GrantType                    string            `json:"grant_type" mapstructure:"grant_type"`
+		Params                       map[string]string `json:"params" mapstructure:"params"`
+		Expiration                   time.Time         `json:"expiration" mapstructure:"expiration"`
+		AbsoluteExpiration           time.Time         `json:"absolute_expiration" mapstructure:"absolute_expiration"`
+		RefreshTokenGraceEnabled     bool              `json:"refreshTokenGraceEnabled" mapstructure:"refresh_token_grace_enabled"`
+		RefreshTokenGraceTTL         time.Duration     `json:"refreshTokenGraceTTL" mapstructure:"refresh_token_grace_ttl"`
+		RefreshTokenGraceMaxAttempts int               `json:"refreshTokenGraceMaxAttempts" mapstructure:"refresh_token_grace_max_attempts"`
+		RefreshTokenGraceAttempts    int               `json:"refreshTokenGraceAttempts" mapstructure:"refresh_token_grace_attempts"`
 	}
 )
 
@@ -41,3 +48,42 @@ const (
 	TokenTypeSubjectToken     = "subject_token"
 	TokenTypeSubjectTokenType = "subject_token_type"
 )
+
+func (s *RefreshTokenInfo) UnmarshalJSON(data []byte) error {
+	var err error
+	var tmp struct {
+		ClientID                     string            `json:"client_id" mapstructure:"client_id"`
+		Subject                      string            `json:"subject" mapstructure:"subject"`
+		Scope                        string            `json:"scope" mapstructure:"scope"`
+		GrantType                    string            `json:"grant_type" mapstructure:"grant_type"`
+		Params                       map[string]string `json:"params" mapstructure:"params"`
+		Expiration                   string            `json:"expiration" mapstructure:"expiration"`
+		AbsoluteExpiration           string            `json:"absolute_expiration" mapstructure:"absolute_expiration"`
+		RefreshTokenGraceEnabled     bool              `json:"refresh_token_grace_enabled" mapstructure:"refresh_token_grace_enabled"`
+		RefreshTokenGraceTTL         time.Duration     `json:"refresh_token_grace_ttl" mapstructure:"refresh_token_grace_ttl"`
+		RefreshTokenGraceMaxAttempts int               `json:"refresh_token_grace_max_attempts" mapstructure:"refresh_token_grace_max_attempts"`
+		RefreshTokenGraceAttempts    int               `json:"refresh_token_grace_attempts" mapstructure:"refresh_token_grace_attempts"`
+	}
+	if err = json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+	s.ClientID = tmp.ClientID
+	s.Subject = tmp.Subject
+	s.Scope = tmp.Scope
+	s.GrantType = tmp.GrantType
+	s.Params = tmp.Params
+	s.Expiration, err = time.Parse(time.RFC3339, tmp.Expiration)
+	if err != nil {
+		return err
+	}
+	s.AbsoluteExpiration, err = time.Parse(time.RFC3339, tmp.AbsoluteExpiration)
+	if err != nil {
+		return err
+	}
+	s.RefreshTokenGraceEnabled = tmp.RefreshTokenGraceEnabled
+	s.RefreshTokenGraceTTL = tmp.RefreshTokenGraceTTL
+	s.RefreshTokenGraceMaxAttempts = tmp.RefreshTokenGraceMaxAttempts
+	s.RefreshTokenGraceAttempts = tmp.RefreshTokenGraceAttempts
+	return nil
+
+}
